9a72b22e40d50f50518d6e877cb0786ec66d4d23
FabioAbdo <fabioabdo@hotmail.com>
2026-03-21 01:46:18 -0300

    fix: import auth module into solutions module guards

--- src/modules/solutions/solutions.module.ts
+++ src/modules/solutions/solutions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SolutionsController } from './solutions.controller';
 import { SolutionsService } from './solutions.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [SolutionsController],
   providers: [SolutionsService],
 })
